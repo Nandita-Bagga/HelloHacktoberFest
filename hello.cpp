a945aa05062e7074bde5a9d76f301a264c24f622
Nandita-Bagga <nnandita_be19@thapar.edu>
2020-10-26 18:43:10 +0530

    modified

---
+++ hello.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Hello World!!";
+    cout<<"Programming is a wholoe new different world for those who understand the essence of it.";
+}
